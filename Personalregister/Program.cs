034e1ca070a9e471b41d3cbda2464e23ff8f7770
Kaisa K <only4kaisa@gmail.com>
2022-05-10 15:57:47 +0200

    Add project files.

---
+++ Personalregister/Program.cs
@@ -0,0 +1,23 @@
+﻿//Uppgift 1: Vilka klasser bör ingå i prorammet
+
+//Uppgift 2: Vilka attribut och metoder bör ingå i dessa klasser
+
+//Uppgift 3: Skriv programmet
+
+Console.WriteLine("Please enter FirstName");
+Console.WriteLine("Please enter LastName");
+Console.WriteLine("Please enter the Salary");
+
+string FirstName = Console.ReadLine();
+string LastName = Console.ReadLine();
+var Salary = Console.ReadLine();
+
+Console.WriteLine("Fullname: " + FirstName + " " + LastName + "," + " Salary: " + Salary);
+
+/* 
+   Detta är min första kod och jag kan inte bättre denna gång 
+   då C# är helt nytt för mig,
+   men ser framemot att lära mig mer.
+*/
+
+
